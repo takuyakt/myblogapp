ad3b4d8270e334caaae27d4fff529e8f63ef8d73
takuyakt <10061798+takuyakt@users.noreply.github.com>
2021-05-04 16:01:57 +0900

    add Post class.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    published = models.DateTimeField()
+    image = models.ImageField(upload_to='media/')
+    body = models.TextField()
+    
