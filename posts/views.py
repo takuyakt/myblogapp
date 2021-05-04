b44a00ae17ccb071bba3f3da01c6133bb7dd5cfb
takuyakt <10061798+takuyakt@users.noreply.github.com>
2021-05-04 15:35:14 +0900

    add index template.

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world! このページは投稿のインデックスです。")
+    # return HttpResponse("Hello world! このページは投稿のインデックスです。")
+    return render(request,'posts/index.html')
 
 # Create your views here.
