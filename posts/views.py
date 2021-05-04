12b2959aeb35ea93f13965cc8a17af6791d5dab9
takuyakt <10061798+takuyakt@users.noreply.github.com>
2021-05-04 12:23:15 +0900

    add index function.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world! このページは投稿のインデックスです。")
 
 # Create your views here.
