f473fda495d37aba73f7dcdaf4431e2cf7cefd0b
takuyakt <10061798+takuyakt@users.noreply.github.com>
2021-05-05 19:47:36 +0900

    WIP: modify index page.

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
     # return HttpResponse("Hello world! このページは投稿のインデックスです。")
-    return render(request,'posts/index.html')
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts' : posts})
 
 # Create your views here.
