df0a7ce529c9905c294212fe80ff41127fe8ca25
takuyakt <10061798+takuyakt@users.noreply.github.com>
2021-07-27 09:27:52 +0900

    add bootstrap to index.html

--- myblogapp/urls.py
+++ myblogapp/urls.py
@@ -22,5 +22,5 @@ from posts import views
 urlpatterns = [
     url(r'^posts/', include('posts.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_detail),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
